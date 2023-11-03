65d314365a53d7e2c3c445a2ece71ab7206c8e8e
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-11-03 17:00:52 -0500

    Initial commit

---
+++ src/main/java/pe/edu/cibertec/DSWII_Grupo10/DswiiGrupo10Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWII_Grupo10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiGrupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiGrupo10Application.class, args);
+	}
+
+}
