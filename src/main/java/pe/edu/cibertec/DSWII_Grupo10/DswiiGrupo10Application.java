26db2769a120a485cd3d245b0ce909222879f30c
Gaxel18 <alejandro_cs_09@hotmail.com>
2023-11-03 18:15:39 -0500

    application.properties aplication

--- src/main/java/pe/edu/cibertec/DSWII_Grupo10/DswiiGrupo10Application.java
+++ src/main/java/pe/edu/cibertec/DSWII_Grupo10/DswiiGrupo10Application.java
@@ -2,12 +2,27 @@ package pe.edu.cibertec.DSWII_Grupo10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DswiiGrupo10Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DswiiGrupo10Application.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer configCorsGlobal(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedMethods("GET", "POST", "PUT")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 }
